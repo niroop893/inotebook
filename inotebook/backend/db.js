d594b67334c202baca3d00d3bbeaa686cedb6573
Shetty <niroop893@gmail.com>
2024-10-27 13:18:15 +0530

    udpated index.js connect to Mongo

---
+++ inotebook/backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb+srv://admin:Aaddbb%401234@cluster0.psz6mz0.mongodb.net/Niroop?retryWrites=true&w=majority";
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("connected to mongodb");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+ 
+    }
+}
+
+module.exports = { connectToMongo };
