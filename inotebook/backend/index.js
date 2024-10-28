b82b39188874f318a29f6a15fddeaf3cd6bc4ef1
Shetty <niroop893@gmail.com>
2024-10-28 12:49:44 +0530

    connect to mongodb and add users

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -4,8 +4,14 @@ const express = require('express')
 
 connectToMongo();
 
-const app = express()
+const app = express();
 const port = 3000
+
+app.use(express.json())
+
+//Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
