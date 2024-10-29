bd57eedec26a016714621087d1e8c45987a09cf1
Shetty <niroop893@gmail.com>
2024-10-29 17:24:21 +0530

    generate jwt token

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -44,7 +44,7 @@ router.post('/createuser',[
         }
         const authToken = jwt.sign(data, JWT_SECRET)
         
-        res.json({authToken})
+        res.json({authToken});
     } catch (error) {
         console.error(error.message)
         res.status(500).send("Some error occured")
@@ -52,4 +52,39 @@ router.post('/createuser',[
     
 })
 
+// Authenticate a user using post: '/api/auth/login' , no login required
+
+router.post('/login',[
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists()
+], async (req, res)=>{
+    const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+
+    const {email, password} = req.body;
+    try{
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error: "Please login with correct credentials"});
+        }
+        const passwordCompare = await bcrypt.compare(password, user.password)
+        if(!passwordCompare){
+                return res.status(400).json({error: "Please login with correct credentials"});
+            }
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            // JWT Token Creation
+        const authToken = await jwt.sign(data, JWT_SECRET)
+            res.json({authToken})
+
+    }  catch (error) {
+        console.error(error.message)
+        res.status(500).send("Internal server error")
+    }
+})
 module.exports = router
